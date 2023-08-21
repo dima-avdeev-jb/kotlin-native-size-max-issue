c85d725b95f89883ebc2b75be9005366e7e6b797
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-21 19:34:24 +0400

    reproduce val SIZE_MAX

---
+++ shared/src/commonMain/kotlin/org/example/App.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    Text("Hello")
+}
+
+const val SIZE_MAX = 80
